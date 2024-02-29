995b8864f2d9aafc8d42e3c9da404b575fc54fb4
Hashika Chathubhashaka <74332679+HashikaChathubhashaka@users.noreply.github.com>
2024-02-29 10:42:51 +0530

    Delete build directory

--- build/game/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/diyazen/hashika_ws/install/game'
--- build/run_game/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/diyazen/hashika_ws/install/game/share/game/package.sh"
